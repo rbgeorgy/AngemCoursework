151a7197790ada408992d4fe13f3ceb46f527536
Георгий Бербат <invariantcalibration@gmail.com>
2020-12-26 23:54:03 +0200

    allow negative and decimal numbers

--- lib/screens/second_order_screen.dart
+++ lib/screens/second_order_screen.dart
@@ -139,13 +139,68 @@ class NumFormField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextFormField(
         controller: cController,
-        keyboardType: TextInputType.number,
+        keyboardType:
+            TextInputType.numberWithOptions(decimal: true, signed: true),
         inputFormatters: <TextInputFormatter>[
-          FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+          WhitelistingTextInputFormatter(RegExp(r'[\d+\-\.]')),
+          NumberTextInputFormatter(decimalRange: 9),
         ],
         decoration: InputDecoration(
           labelText: label,
           hintText: "",
         ));
+  }
+}
+
+class NumberTextInputFormatter extends TextInputFormatter {
+  NumberTextInputFormatter({this.decimalRange})
+      : assert(decimalRange == null || decimalRange > 0);
+
+  final int decimalRange;
+
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    TextEditingValue _newValue = this.sanitize(newValue);
+    String text = _newValue.text;
+
+    if (decimalRange == null) {
+      return _newValue;
+    }
+
+    if (text == '.') {
+      return TextEditingValue(
+        text: '0.',
+        selection: _newValue.selection.copyWith(baseOffset: 2, extentOffset: 2),
+        composing: TextRange.empty,
+      );
+    }
+
+    return this.isValid(text) ? _newValue : oldValue;
+  }
+
+  bool isValid(String text) {
+    int dots = '.'.allMatches(text).length;
+
+    if (dots == 0) {
+      return true;
+    }
+
+    if (dots > 1) {
+      return false;
+    }
+
+    return text.substring(text.indexOf('.') + 1).length <= decimalRange;
+  }
+
+  TextEditingValue sanitize(TextEditingValue value) {
+    if (false == value.text.contains('-')) {
+      return value;
+    }
+
+    String text = '-' + value.text.replaceAll('-', '');
+
+    return TextEditingValue(
+        text: text, selection: value.selection, composing: TextRange.empty);
   }
 }
